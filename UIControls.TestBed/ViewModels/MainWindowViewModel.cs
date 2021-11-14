2e02349496458b6661b975a905661b346239c140
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-11-14 01:54:44 +0100

    initial commit

---
+++ UIControls.TestBed/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using ReactiveUI;
+
+namespace UIControls.TestBed.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase, INotifyDataErrorInfo
+    {
+        private string greeting = string.Empty;
+
+        private string _greetingsError = string.Empty;
+
+        public string Greeting 
+        { 
+            get => greeting; 
+            set => this.RaiseAndSetIfChanged( ref greeting, value); 
+        }
+
+        public bool HasErrors => throw new NotImplementedException();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public MainWindowViewModel()
+        {
+            this.WhenAnyValue(x => x.Greeting)
+                .Subscribe(_ => this.UpdateErrors());
+        }
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            switch(propertyName)
+            {
+                case nameof(this.Greeting):
+                    return new[] { this._greetingsError };
+                default:
+                    return null;
+            }
+        }
+
+        private void UpdateErrors()
+        {
+            if (string.IsNullOrEmpty(this.Greeting))
+            {
+                this._greetingsError = "TextBox cannot be empty!";
+                this.ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(this.Greeting)));
+            }
+            else
+            {
+                this._greetingsError = null;
+                this.ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(this.Greeting)));
+            }
+        }
+    }
+}
---
+++ UIControls/Badge/Badge.cs
@@ -0,0 +1,34 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Styling;
+
+namespace UIControls
+{
+    public class Badge : ContentControl, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(ContentControl);
+
+        public static readonly StyledProperty<object> BadgeContentProperty =
+            AvaloniaProperty.Register<Badge, object>(nameof(BadgeContent));
+        public static readonly StyledProperty<IDataTemplate> BadgeContentTemplateProperty =
+            AvaloniaProperty.Register<Badge, IDataTemplate>(nameof(BadgeContentTemplate));
+
+        public object BadgeContent
+        {
+            get => GetValue(BadgeContentProperty);
+            set => SetValue(BadgeContentProperty, value);
+        }
+
+        public IDataTemplate BadgeContentTemplate
+        {
+            get => GetValue(BadgeContentTemplateProperty);
+            set => SetValue(BadgeContentTemplateProperty, value);
+        }
+
+        static Badge()
+        {
+            ClipToBoundsProperty.OverrideDefaultValue<Badge>(false);
+        }
+    }
+}
